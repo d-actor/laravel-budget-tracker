bf217c86359455a988fb82388fa03b3222df2974
Dan Actor <danltactor@gmail.com>
2019-12-06 01:18:24 -0700

    added postgres database configuration

--- config/database.php
+++ config/database.php
@@ -1,4 +1,5 @@
 <?php
+$DATABASE_URL=parse_url('postgres://boihhkqnfzxciw:33507cf24112e790f619f96c5bfa757ac77aeaa9acff67d50625be556ad98053@ec2-184-73-169-163.compute-1.amazonaws.com:5432/d44ik5s2lu3300');
 
 use Illuminate\Support\Str;
 
@@ -15,7 +16,8 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
+
 
     /*
     |--------------------------------------------------------------------------
@@ -65,17 +67,15 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
-            'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require',
         ],
 
         'sqlsrv' => [

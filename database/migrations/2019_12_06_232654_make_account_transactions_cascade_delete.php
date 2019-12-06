31fb0f8ef6770ec328975ec037ad1c43424cc568
Dan Actor <danltactor@gmail.com>
2019-12-06 16:43:22 -0700

    cascade delete on transactions/accounts

---
+++ database/migrations/2019_12_06_232654_make_account_transactions_cascade_delete.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class MakeAccountTransactionsCascadeDelete extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('transactions', function ($table) {      
+            $table->dropForeign(['account_id']);
+
+            $table->foreign('account_id')->references('id')->on('accounts')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
